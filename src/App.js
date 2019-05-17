8d8593d926155d9fbc14c9e6dc405821b28b6786
Johnson Fu <johnson.fu@chubb.com>
2019-05-17 16:45:57 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import './TodoList';
+import TodoList from './TodoList';
+import TodoItems from './TodoItems';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: { text: 'abcd', key: '' },
+    }
+    this.inputElement = React.createRef();
+  }
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }  
+
+  handleInput = e => {
+    //console.log('Hello Input');
+    //console.log(this.state.currentItem);
+
+    const itemText = e.target.value
+    console.log('itemText: ' + e.target.value);
+
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+
+  addItem = e => {
+
+    e.preventDefault();
+
+    console.log('Hello Add Item')
+
+    const newItem = this.state.currentItem;
+
+    if (newItem.text !== '') {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
+        <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
+      </div>
+    )
+  }
 }
 
 export default App;
